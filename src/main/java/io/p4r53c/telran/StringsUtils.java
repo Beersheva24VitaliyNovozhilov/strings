fe0d4b1afe3ae422a33b60c0e9e664ee5af5abc2
p4r53c <n1chr0mix@gmail.com>
2024-07-25 22:42:03 +0300

    Encapsulate data and exps into methods

--- src/main/java/io/p4r53c/telran/StringsUtils.java
+++ src/main/java/io/p4r53c/telran/StringsUtils.java
@@ -1,5 +1,7 @@
 package io.p4r53c.telran;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class StringsUtils {
@@ -12,12 +14,20 @@ public class StringsUtils {
      * expression and a set of keywords.
      *
      * @param variableName the string to check
-     * @param regex        the regular expression used to validate the variable name
-     * @param keywords     the set of keywords to check against
      * @return true if the variable name is valid, false otherwise
      */
-    public static boolean isValidJavaVariableName(String variableName, String regex, Set<String> keywords) {
-        return variableName.matches(regex) && !keywords.contains(variableName);
+    public static boolean isValidJavaVariableName(String variableName) {
+
+        // https://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+        Set<String> javaKeywords = new HashSet<>(Arrays.asList(
+                "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized",
+                "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw",
+                "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient",
+                "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void",
+                "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"));
+
+        String validationRegex = "^[a-zA-Z_$][a-zA-Z\\d_$]*$";
+        return variableName.matches(validationRegex) && !javaKeywords.contains(variableName);
     }
 
     /**
@@ -25,11 +35,10 @@ public class StringsUtils {
      * conventional Java variable name.
      *
      * @param variableName the string to be checked
-     * @param regex        the regular expression pattern to match against
      * @return true if the string matches the pattern, false otherwise
      */
-    public static boolean isConventionalJavaVariableName(String variableName, String regex) {
-        return variableName.matches(regex);
+    public static boolean isConventionalJavaVariableName(String variableName) {
+        String conventionalRegex = "^[a-z]+([A-Z][a-z]*)*$";
+        return variableName.matches(conventionalRegex);
     }
-
 }
