c0e76b277fa5135c44626fcb0e222f5ef9601657
p4r53c <n1chr0mix@gmail.com>
2024-07-26 19:21:50 +0300

    Fix variable pattern

--- src/test/java/io/p4r53c/telran/StringsTest.java
+++ src/test/java/io/p4r53c/telran/StringsTest.java
@@ -11,46 +11,52 @@ import org.junit.jupiter.api.Test;
 
 class StringsTest {
 
-    @Test
-    void testFirstName() {
-        String regex = Strings.firstName();
+        @Test
+        void testFirstName() {
+                String regex = Strings.firstName();
 
-        String[] validNames = { "Vitaliy", "Vitaly" };
-        String[] invalidNames = { "Vitaly1", "Vitaliy2", "" };
+                String[] validNames = { "Vitaliy", "Vitaly" };
+                String[] invalidNames = { "Vitaly1", "Vitaliy2", "" };
 
-        Arrays.stream(validNames).forEach(name -> assertTrue(name.matches(regex), "Expected valid: " + name));
-        Arrays.stream(invalidNames).forEach(name -> assertFalse(name.matches(regex), "Expected invalid: " + name));
-    }
+                Arrays.stream(validNames).forEach(name -> assertTrue(name.matches(regex), "Expected valid: " + name));
+                Arrays.stream(invalidNames)
+                                .forEach(name -> assertFalse(name.matches(regex), "Expected invalid: " + name));
+        }
 
-    @Test
-    void testJavaVariable() {
-        String regex = Strings.javaVariable();
+        @Test
+        void testJavaVariable() {
+                String regex = Strings.javaVariable();
 
-        String[] validVariableNames = { "variable1", "variable", "my_var", "isSynchronized", "__", "____", "$var" };
-        String[] invalidVariableNames = { "1variable", "int", "", "synchronized", "_", ".var", "_", "v@r", "@var", "~var", "var~" };
+                String[] validVariableNames = { "variable1", "variable", "my_var", "isSynchronized", "__", "____",
+                                "$var" };
+                                
+                String[] invalidVariableNames = { "1variable", "int", "", "synchronized", "_", ".var", "v@r",
+                                "@var", "~var", "var~" };
 
-        Arrays.stream(validVariableNames)
-                .forEach(variableName -> assertTrue(
-                        variableName.matches(regex) && !SourceVersion.isKeyword(variableName, SourceVersion.latest()),
-                        "Expected valid: " + variableName));
+                Arrays.stream(validVariableNames)
+                                .forEach(variableName -> assertTrue(
+                                                variableName.matches(regex) && !SourceVersion.isKeyword(variableName,
+                                                                SourceVersion.latest()),
+                                                "Expected valid: " + variableName));
 
-        Arrays.stream(invalidVariableNames)
-                .forEach(variableName -> assertFalse(
-                        variableName.matches(regex) && !SourceVersion.isKeyword(variableName, SourceVersion.latest()),
-                        "Expected invalid: " + variableName));
-    }
+                Arrays.stream(invalidVariableNames)
+                                .forEach(variableName -> assertFalse(
+                                                variableName.matches(regex) && !SourceVersion.isKeyword(variableName,
+                                                                SourceVersion.latest()),
+                                                "Expected invalid: " + variableName));
+        }
 
-    @Test
-    void testConventionalJavaVariable() {
-        String regex = Strings.conventionalJavaVariable();
+        @Test
+        void testConventionalJavaVariable() {
+                String regex = Strings.conventionalJavaVariable();
 
-        String[] validVariableNames = { "variableName", "variablename" };
-        String[] invalidVariableNames = { "VariableName", "variable_name", "" };
+                String[] validVariableNames = { "variableName", "variablename" };
+                String[] invalidVariableNames = { "VariableName", "variable_name", "" };
 
-        Arrays.stream(validVariableNames).forEach(variableName -> assertTrue(variableName.matches(regex),
-                "Expected valid: " + variableName));
+                Arrays.stream(validVariableNames).forEach(variableName -> assertTrue(variableName.matches(regex),
+                                "Expected valid: " + variableName));
 
-        Arrays.stream(invalidVariableNames).forEach(variableName -> assertFalse(variableName.matches(regex),
-                "Expected invalid: " + variableName));
-    }
+                Arrays.stream(invalidVariableNames).forEach(variableName -> assertFalse(variableName.matches(regex),
+                                "Expected invalid: " + variableName));
+        }
 }
