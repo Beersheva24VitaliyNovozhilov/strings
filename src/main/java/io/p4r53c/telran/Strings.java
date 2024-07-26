43407c8fed77b0edd4aff7f4b843f378fc0f0766
p4r53c <n1chr0mix@gmail.com>
2024-07-26 17:37:23 +0300

    Add javadoc

--- src/main/java/io/p4r53c/telran/Strings.java
+++ src/main/java/io/p4r53c/telran/Strings.java
@@ -5,14 +5,32 @@ public class Strings {
     private Strings() {
     }
 
+    /**
+     * Returns a regular expression pattern for a valid first name. The pattern
+     * matches a string that starts with an uppercase letter followed by 4 or more
+     * lowercase letters.
+     *
+     * @return a string representing the regular expression pattern
+     */
     public static String firstName() {
         return "[A-Z][a-z]{4,}";
     }
 
+    /**
+     * Returns a regular expression pattern for a valid Java variable name.
+     *
+     * @return a string representing the regular expression pattern
+     */
     public static String javaVariable() {
         return "^[a-zA-Z_$][a-zA-Z\\d_$]*$";
     }
 
+    /**
+     * Returns a regular expression pattern for a valid conventional Java variable
+     * name.
+     *
+     * @return a string representing the regular expression pattern
+     */
     public static String conventionalJavaVariable() {
         return "^[a-z]+([A-Z][a-z]*)*$";
     }
